81b00b1c82af797da6164461c097acce81d676bb
dlrdyd97 <dlrdyd97@naver.com>
2023-04-27 10:44:18 +0900

    깃연동1차

---
+++ src/Map/LandingPage.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Kakao from "./Kakao";
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  let navigate = useNavigate();
+
+  const [InputText, setInputText] = useState("");
+  const [Place, setPlace] = useState("");
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPlace(InputText);
+    setInputText("");
+  };
+
+  return (
+    <>
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input
+          placeholder="검색어를 입력하세요"
+          onChange={onChange}
+          value={InputText}
+        />
+        <button type="submit">검색</button>
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          돌아가기
+        </button>
+      </form>
+      <Kakao searchPlace={Place} />
+    </>
+  );
+};
+
+export default LandingPage;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Components/Header.js";
 import Footer from "../Components/Footer.js";
 import "../Styles/Main.css";
+import "../Styles/MainIh.css";
+import { useNavigate } from "react-router-dom";
+import AccompanyingList from "../Components/AccompanyingList.js";
+import Kakao from "../Map/Kakao.js";
 
 const Home = () => {
+  let navigate = useNavigate();
+
+  const [alist, setAlist] = useState([
+    { 지역: "서울", 시설명: "XX카페", 카테고리: "카페" },
+    { 지역: "인천", 시설명: "XX공원", 카테고리: "공원" },
+    { 지역: "강원도", 시설명: "XX음식점", 카테고리: "음식점" },
+  ]);
+
+  const test123 = () => {
+    setAlist([
+      ...alist,
+      {
+        지역: "경기",
+        시설명: "XX병원",
+        카테고리: "병원",
+      },
+    ]);
+  };
   return (
     <section className="full-bg">
       <section className="left-bg">
@@ -26,8 +48,33 @@ const Home = () => {
       <main>
         <Header />
         <section>
-          {/* 여기에 모바일 화면 
-          개발하시면 됩니다*/}
+          {/* 메인 페이지 개발 */}
+          <div className="containerIh">
+            <h2 align="center">메인페이지</h2>
+            <div className="testmap">
+              <h3 align="center">지도</h3>
+              <button
+                onClick={() => {
+                  navigate("/map");
+                }}
+              >
+                지도 페이지 가기
+              </button>
+            </div>
+            <div className="testlist">
+              <h4 align="center">주변 반려동물 동반 가능 장소</h4>
+              {alist.map((list, index) => {
+                return (
+                  <div className="mainlist">
+                    <AccompanyingList key={index} list={list} />
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              <button onClick={test123}>리스트 추가</button>
+            </div>
+          </div>
         </section>
         <Footer />
       </main>
