4f7bbc38c9ec05e8db2a9fb4c56cbc7c5b83772a
majorTom-cat <ysk.skyer@gmail.com>
2023-04-27 10:30:02 +0900

    여행추천 업데이트, 구조 수정
    1. Recomend, 관련 컴포넌트 및 이미지, 스타일 업데이트
    2. 스크롤 기능을 위한 스타일 추가
    3. Footer 컴포넌트 최하단에 배치 위해 위치 변경및 position: absolute 에서 relative로 수정

--- src/Pages/RouteTrip.js
+++ src/Pages/RouteTrip.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Components/Header.js";
 import Footer from "../Components/Footer.js";
 import "../Styles/Main.css";
+import Recomend from "../Components/Recomend.js";
 
 const RouteTrip = () => {
   return (
@@ -23,14 +24,13 @@ const RouteTrip = () => {
         </div>
       </section>
 
-      <main>
+      <main className="RouteTripSection">
         <Header />
         <section>
-          {/* 여기에 모바일 화면 
-          개발하시면 됩니다*/}
-          <p>여행 추천 페이지입니다</p>
+          <Recomend />
+          {/* <p>여행 추천 페이지입니다</p> */}
         </section>
-        <Footer />
+        {/* <Footer /> */}
       </main>
     </section>
   );
