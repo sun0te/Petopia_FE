6b6bc940bdd1016cc45054a2843a994f286e248e
sun0te <jin1123a@gmail.com>
2023-04-28 17:13:39 +0900

    모든 페이지 배경 컴포넌트 적용

---
+++ src/Components/PostEdit.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const PostEdit = ({ page }) => {
+  return {
+    /*글 수정 */
+  };
+};
+
+export default PostEdit;
--- src/Pages/UserRegionBoard.js
+++ src/Pages/UserRegionBoard.js
@@ -1,27 +1,12 @@
 import React from "react";
 import Header from "../Components/Header.js";
 import Footer from "../Components/Footer.js";
-import "../Styles/Main.css";
+import BgLeft from "../Components/BgLeft.js";
 
 const UserRegionBoard = () => {
   return (
-    <section className="full-bg">
-      <section className="left-bg">
-        {/*배경 요소 디자인 */}
-        <div className="logo">
-          <img src="../../img/logo.png" alt="Petoia logo" />
-        </div>
-        <div className="main-left">
-          <h1>
-            반려 동물과
-            <br />
-            함께하는
-            <br />
-            일상 여행
-          </h1>
-          <img src="../../img/dog_main.png" alt="dog" />
-        </div>
-      </section>
+    <>
+      <BgLeft />
 
       <main>
         <Header />
@@ -32,7 +17,7 @@ const UserRegionBoard = () => {
         </section>
         <Footer />
       </main>
-    </section>
+    </>
   );
 };
 
