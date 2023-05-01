753fdf7246e16889a3aa563efc2545faa971c386
sun0te <jin1123a@gmail.com>
2023-05-01 10:08:03 +0900

    [Add] 슬라이드 라이브러리, 홈 슬라이드 틀 생성

--- src/Components/BgLeft.js
+++ src/Components/BgLeft.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Styles/Main.css";
+import "../Styles/BgLeft.css";
 
 const BgLeft = () => {
   return (
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,53 @@
 import React from "react";
+import Slider from "react-slick";
+import "../Styles/Home.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Header from "../Components/Header.js";
 import Footer from "../Components/Footer.js";
 import BgLeft from "../Components/BgLeft.js";
 
+const slides = [
+  {
+    id: 1,
+    image: "https://placehold.it/360x240",
+    caption: "First slide",
+  },
+  {
+    id: 2,
+    image: "https://placehold.it/360x240",
+    caption: "Second slide",
+  },
+  {
+    id: 3,
+    image: "https://placehold.it/360x240",
+    caption: "Third slide",
+  },
+];
+
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <BgLeft />
       <main>
         <Header />
         <section>
-          {/* 여기에 모바일 화면
-    개발하시면 됩니다*/}
+          <Slider {...settings}>
+            {slides.map((slide) => (
+              <div key={slide.id}>
+                <img src={slide.image} alt={slide.caption} />
+                <div className="caption">{slide.caption}</div>
+              </div>
+            ))}
+          </Slider>
         </section>
         <Footer />
       </main>
