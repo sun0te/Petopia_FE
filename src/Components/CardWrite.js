dd2a657c309e60b8d62fb53f7cc5d27344865ec1
essential14 <essential193@gmail.com>
2023-05-02 17:24:48 +0900

    [ADD] 스크롤,간격 수정

---
+++ src/Components/CardWrite.js
@@ -0,0 +1,120 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "../Styles/CardWrite.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useEffect, useState, useRef } from "react";
+import { BsTrash3 } from "react-icons/bs";
+import ListGroup from "react-bootstrap/ListGroup";
+
+const CardWrite = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const inputRef = useRef(null);
+
+  const handleFileInputChange = (event) => {
+    setSelectedFiles([...selectedFiles, ...event.target.files]);
+  };
+
+  const handleRemoveImage = (index) => {
+    const newFiles = [...selectedFiles];
+    newFiles.splice(index, 1);
+    setSelectedFiles(newFiles);
+  };
+
+  const handleUploadClick = () => {
+    console.log(selectedFiles);
+  };
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <>
+      <div className="writeForm">
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label></Form.Label>
+            <Form.Control
+              className="writeTitle"
+              type="email"
+              placeholder="글 제목을 입력하세요"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3 writeFormContent">
+            <Form.Label></Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="글 내용을 입력하세요"
+              className="contentForm"
+            />
+          </Form.Group>
+        </Form>
+      </div>
+
+      <div>
+        <div>
+          <div className="uploadBtn">
+            <Button
+              variant="outline-secondary"
+              className=""
+              onClick={handleClick}
+            >
+              <img className="uploadBtnImg" src="img/uploading.png" alt="" />
+            </Button>
+          </div>
+
+          <input
+            className="uploadInput"
+            type="file"
+            multiple
+            onChange={handleFileInputChange}
+            ref={inputRef}
+          />
+        </div>
+
+        <div className="uploadImgDiv">
+          <ListGroup>
+            {selectedFiles.map((file, index) => (
+              <ListGroup.Item className="listGroupItem">
+                <div key={index}>
+                  <img
+                    className="uploadImg"
+                    src={URL.createObjectURL(file)}
+                    alt={`${file.name}`}
+                  />
+                  <p className="imgTitle">{file.name}</p>
+                  <div className="imgDeleteBtnDiv">
+                    <button
+                      className="imgDeleteBtn"
+                      onClick={() => handleRemoveImage(index)}
+                    >
+                      <BsTrash3 />
+                    </button>
+                  </div>
+                </div>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+        <div className="btns">
+          <button
+            className="btn btn-sm btn-outline-primary submit"
+            onClick={handleUploadClick}
+          >
+            작성 완료
+          </button>
+          <button
+            type="button"
+            className="btn btn-sm btn-outline-primary boardListBtn"
+          >
+            글목록
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardWrite;
