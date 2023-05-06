ba5c0853b8ccbd2228953d4ec4e8752d7ace1f6f
essential14 <essential193@gmail.com>
2023-05-06 22:58:28 +0900

    [ADD] search 기능 구현, 구조 수정

--- src/Components/SearchBarcm.js
+++ src/Components/SearchBarcm.js
@@ -4,60 +4,32 @@ import axios from "axios";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
-const SearchBarcm = () => {
-  const [search, setsearch] = useState([
-    {
-      id: 5,
-      title: "테스트입니다",
-      content: "테스트",
-      date: "2023-04-25",
-      author: "관리자2",
-      views: 10,
-      comments: 2,
-      thumbnailUrl: "https://via.placeholder.com/150",
-    },
-  ]);
-
-  useEffect(() => {
-    axios
-      .get("/search")
-      .then((response) => {
-        setsearch(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
+const SearchBarcm = ({ onSearch }) => {
   const handleSearch = (event) => {
-    // 검색 기능 추후 작성
-    console.log("검색어: ", event.target.value);
+    const query = event.target.value.trim();
+    onSearch(query);
   };
 
   return (
-    <>
-      {search.map((search) => (
-        <div style={{ position: "relative" }}>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="검색할 내용을 입력하세요"
-              className="me-1 w-75 searchBar"
-              aria-label="Search"
-              onChange={handleSearch}
-            />
-            <Button
-              className="searchBtn"
-              variant="outline-primary"
-              size="sm"
-              onClick={handleSearch}
-            >
-              검색
-            </Button>
-          </Form>
-        </div>
-      ))}
-    </>
+    <div style={{ position: "relative" }}>
+      <Form className="d-flex">
+        <Form.Control
+          type="search"
+          placeholder="검색할 내용을 입력하세요"
+          className="me-1 w-75 searchBar"
+          aria-label="Search"
+          onChange={handleSearch}
+        />
+        <Button
+          className="searchBtn"
+          variant="outline-primary"
+          size="sm"
+          onClick={handleSearch}
+        >
+          검색
+        </Button>
+      </Form>
+    </div>
   );
 };
 
