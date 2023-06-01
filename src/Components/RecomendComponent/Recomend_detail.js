4e850a916f5e930d274bd9f9b002a8d712d5c9ee
majorTom-cat <ysk.skyer@gmail.com>
2023-06-01 15:09:18 +0900

    1

--- src/Components/RecomendComponent/Recomend_detail.js
+++ src/Components/RecomendComponent/Recomend_detail.js
@@ -509,7 +509,7 @@ const Recomend_detail = () => {
             </div>
           ) : null}
 
-          <Comment />
+          {/* <Comment />
 
           <div className="writeCommentDiv">
             <Form.Group className="mb-3 writeFormContent">
@@ -531,7 +531,7 @@ const Recomend_detail = () => {
                 댓글달기
               </Button>
             </Form.Group>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
